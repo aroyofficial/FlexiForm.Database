155450adcfedd32e369429e2f7b5fc80e1744550
devSushmita <sushmita7156@gmail.com>
2025-06-29 16:12:48 +0530

    Update related procedures #5

---
+++ FlexiForm.Database/Scripts/StoredProcedures/Down/20250629090545753_proc_usp_deleteuser.sql
@@ -0,0 +1,18 @@
+﻿-- Script Type    : proc
+-- Name           : 20250629090545753_proc_usp_deleteuser.sql
+-- Created At     : 2025-06-29 09:05:45 UTC (Sushmita Das)
+-- Script ID      : 20250629090545753
+-- Migration Type : Down
+
+BEGIN TRY
+    BEGIN TRANSACTION;
+
+    DROP PROCEDURE
+    IF EXISTS usp_DeleteUser;
+
+    COMMIT;
+END TRY
+BEGIN CATCH
+    ROLLBACK;
+    PRINT 'An error occurred: ' + ERROR_MESSAGE();
+END CATCH
---
+++ FlexiForm.Database/Scripts/StoredProcedures/Up/20250629090545753_proc_usp_deleteuser.sql
@@ -0,0 +1,49 @@
+﻿-- Script Type    : proc
+-- Name           : 20250629090545753_proc_usp_deleteuser.sql
+-- Created At     : 2025-06-29 09:05:45 UTC (Sushmita Das)
+-- Script ID      : 20250629090545753
+-- Migration Type : Up
+
+BEGIN TRY
+    BEGIN TRANSACTION;
+
+    DROP PROCEDURE
+    IF EXISTS usp_DeleteUser;
+
+    DECLARE @L_SQL NVARCHAR(MAX) = '
+    CREATE PROCEDURE usp_DeleteUser @id UNIQUEIDENTIFIER,
+    WITH ENCRYPTION
+    AS
+      BEGIN
+          BEGIN TRY
+              SET NOCOUNT ON;
+              
+              DECLARE @L_Id UNIQUEIDENTIFIER = @id;
+              DECLARE @L_Active BIT = 0;
+              DECLARE @L_Deleted BIT = 1;
+
+              UPDATE tblUsers
+              SET Void = @L_Deleted
+              WHERE  RowId = @L_Id 
+              AND Void = @L_Active;
+
+              SET NOCOUNT OFF;
+
+              RETURN;
+          END TRY
+          BEGIN CATCH
+              SET NOCOUNT OFF;
+              DECLARE @L_ErrorMessage VARCHAR(512) = Error_message();
+              RAISERROR(@L_ErrorMessage,16,1);
+              THROW
+          END CATCH
+      END';
+
+      EXEC sp_executesql @L_SQL;
+
+    COMMIT;
+END TRY
+BEGIN CATCH
+    ROLLBACK;
+    PRINT 'An error occurred: ' + ERROR_MESSAGE();
+END CATCH
