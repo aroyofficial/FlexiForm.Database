eec892e2171d5b4ed8c6007222d9db738a6f35e3
Arijit Roy <aroy02072000@gmail.com>
2025-07-05 00:41:22 +0530

    Add usp_GetOTP #31

---
+++ FlexiForm.Database/Scripts/StoredProcedures/Down/20250704181549080_proc_usp_getotp.sql
@@ -0,0 +1,18 @@
+﻿-- Script Type    : proc
+-- Name           : 20250704181549080_proc_usp_getotp.sql
+-- Created At     : 2025-07-04 18:15:49 UTC (Arijit Roy)
+-- Script ID      : 20250704181549080
+-- Migration Type : Down
+
+BEGIN TRY
+    BEGIN TRANSACTION;
+
+    DROP PROCEDURE
+    IF EXISTS usp_GetOTP;
+
+    COMMIT;
+END TRY
+BEGIN CATCH
+    ROLLBACK;
+    PRINT 'An error occurred: ' + ERROR_MESSAGE();
+END CATCH
---
+++ FlexiForm.Database/Scripts/StoredProcedures/Up/20250704181549080_proc_usp_getotp.sql
@@ -0,0 +1,61 @@
+﻿-- Script Type    : proc
+-- Name           : 20250704181549080_proc_usp_getotp.sql
+-- Created At     : 2025-07-04 18:15:49 UTC (Arijit Roy)
+-- Script ID      : 20250704181549080
+-- Migration Type : Up
+
+BEGIN TRY
+    BEGIN TRANSACTION;
+
+    DROP PROCEDURE
+    IF EXISTS usp_GetOTP;
+
+    DECLARE @L_SQL NVARCHAR(MAX) = '
+    CREATE PROCEDURE usp_GetOTP @value VARCHAR(64),
+                                @userid UNIQUEIDENTIFIER
+    WITH ENCRYPTION
+    AS
+    BEGIN
+        BEGIN TRY
+            SET NOCOUNT ON;
+
+            DECLARE @L_Value VARCHAR(64) = @value;
+            DECLARE @L_UserId UNIQUEIDENTIFIER = @userid;
+            DECLARE @L_CurrentUTC DATETIME = GETUTCDATE();
+            DECLARE @L_New TINYINT = 1;
+
+            SELECT TOP 1 Id,
+                         RowId,
+                         Value,
+                         Status,
+                         GeneratedAt,
+                         ExpiredAt,
+                         CreatedAt,
+                         UpdatedAt,
+                         CreatedBy,
+                         UpdatedBy
+            FROM tblOTP
+            WHERE UserId = @L_UserId
+            AND Value = @L_Value
+            AND @L_CurrentUTC BETWEEN CreatedAt AND ExpiredAt
+            AND Status = @L_New;
+
+            SET NOCOUNT OFF;
+            RETURN;
+        END TRY
+        BEGIN CATCH
+            SET NOCOUNT OFF;
+            DECLARE @L_ErrorMessage VARCHAR(512) = Error_message();
+            RAISERROR(@L_ErrorMessage,16,1);
+            THROW
+        END CATCH
+    END';
+
+    EXEC sp_executesql @L_SQL;
+
+    COMMIT;
+END TRY
+BEGIN CATCH
+    ROLLBACK;
+    PRINT 'An error occurred: ' + ERROR_MESSAGE();
+END CATCH
