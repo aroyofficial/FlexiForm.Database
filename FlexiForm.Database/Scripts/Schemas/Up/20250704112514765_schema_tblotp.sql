5c86128e7550d41fd561c869a7db515e4c98587e
devSushmita <sushmita7156@gmail.com>
2025-07-04 17:38:27 +0530

    Add tblOTP #24

---
+++ FlexiForm.Database/Scripts/Schemas/Up/20250704112514765_schema_tblotp.sql
@@ -0,0 +1,34 @@
+﻿-- Script Type    : schema
+-- Name           : 20250704112514765_schema_tblotp.sql
+-- Created At     : 2025-07-04 11:25:14 UTC (Sushmita Das)
+-- Script ID      : 20250704112514765
+-- Migration Type : Up
+
+BEGIN TRY
+    BEGIN TRANSACTION;
+
+     IF Object_id('tblOTP', 'U') IS NULL
+    BEGIN
+        CREATE TABLE tblOTP
+        (
+            Id             INT IDENTITY(1, 1) NOT NULL,
+            RowId          UNIQUEIDENTIFIER DEFAULT Newid() NOT NULL,
+            Value          VARCHAR(64) NOT NULL,
+            Status         TINYINT NOT NULL,
+            GeneratedAt    DATETIME,
+            ExpiredAt      DATETIME,
+            ConsumedAt     DATETIME NULL,
+            CreatedAt      DATETIME,
+            UpdatedAt      DATETIME NULL,
+            CreatedBy      UNIQUEIDENTIFIER, 
+            UpdatedBy      UNIQUEIDENTIFIER NULL
+        );
+    END
+    
+
+    COMMIT;
+END TRY
+BEGIN CATCH
+    ROLLBACK;
+    PRINT 'An error occurred: ' + ERROR_MESSAGE();
+END CATCH
